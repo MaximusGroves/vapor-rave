526a5dbd7fbbcf270d4b22ff575a9d968a5b0fe8
Maximus Groves <max.groves@ncr.com>
2022-04-21 00:51:05 -0400

    color tween

--- src/App.js
+++ src/App.js
@@ -1,101 +1,43 @@
 import logo from "./logo.svg";
-import { Svg, Line } from "react-svg-path";
 import "./App.css";
 import React from "react";
+import useWindowSize from "./useWindowSize";
+import HorizLines from "./HorizLines";
+import VertLines from "./VertLines";
 
 function App() {
-  const screenWidth = window.innerWidth;
-  const screenHeight = window.innerHeight;
+  const windowSize = useWindowSize();
 
-  const horizon = screenHeight * 0.55;
-  const midWidth = screenWidth * 0.5;
+  const screenWidth = windowSize.width || window.innerWidth;
+  const screenHeight = windowSize.height || window.innerHeight;
 
-  const horizTotal = 40;
-  const vertHalf = 25;
-
-  const logYFunc = (val) => 150 * Math.log(val * 7) + (50 - val) * 2;
-
-  const logTopFunc = (val) => 200 * Math.log(val + 2);
-  const logBottomFunc = (val) => 4000 * Math.log(val + 2);
-
-  const lineYArray = [...Array(horizTotal).keys()]
-    .reverse()
-    .map((val) => logYFunc(val));
-  const at50 = lineYArray[0];
-
-  const topXArray = [...Array(vertHalf).keys()].map((val) => logTopFunc(val));
-  const bottomXArray = [...Array(vertHalf).keys()].map((val) =>
-    logBottomFunc(val)
-  );
-  const at20Top = logTopFunc(vertHalf);
-  const at20Bottom = logBottomFunc(vertHalf);
+  const horizon =
+    screenHeight >= 1400 ? screenHeight - 1400 * 0.45 : screenHeight * 0.55;
 
   return (
-    <div className="App">
+    <div
+      className="App"
+      style={{
+        width: windowSize.width,
+        height: windowSize.height,
+        overflow: "hidden",
+        position: "absolute",
+        top: 0,
+        left: 0,
+      }}
+    >
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Svg width={screenWidth} height={screenHeight}>
-          {/* <Line
-            sy={horizon + 5}
-            ey={screenHeight}
-            sx={midWidth + 100}
-            ex={screenWidth}
-            strokeWidth={1}
-            filter={`drop-shadow(${10}px ${10}px ${200}px #ff00ff) drop-shadow(-${10}px -${10}px ${50}px #ff00ff)`}
-          />
-          <Line
-            sy={horizon + 5}
-            ey={screenHeight}
-            sx={midWidth - 100}
-            ex={0}
-            strokeWidth={1}
-            filter={`drop-shadow(${10}px ${10}px ${200}px #ff00ff) drop-shadow(-${10}px -${10}px ${50}px #ff00ff)`}
-          /> */}
-
-          {topXArray.map((val, idx) => (
-            <Line
-              sy={horizon + 5 - idx / 3}
-              ey={screenHeight}
-              sx={midWidth + at20Top - val}
-              ex={midWidth + at20Bottom - bottomXArray[idx]}
-              strokeWidth={`1/${idx + 1}px`}
-              filter={`drop-shadow(${10}px ${10}px ${200}px #ff00ff) drop-shadow(-${10}px -${10}px ${50}px #ff00ff)`}
-            />
-          ))}
-          {topXArray.map((val, idx) => (
-            <Line
-              sy={horizon + 5 - idx / 3}
-              ey={screenHeight}
-              sx={midWidth - at20Top + val}
-              ex={midWidth - at20Bottom + bottomXArray[idx]}
-              strokeWidth={`1/${idx + 1}px`}
-              filter={`drop-shadow(${10}px ${10}px ${200}px #ff00ff) drop-shadow(-${10}px -${10}px ${50}px #ff00ff)`}
-            />
-          ))}
-          <Line
-            sy={horizon - 1}
-            ey={screenHeight}
-            sx={midWidth}
-            ex={midWidth}
-            strokeWidth={1}
-            filter={`drop-shadow(${10}px ${10}px ${200}px #ff00ff) drop-shadow(-${10}px -${10}px ${50}px #ff00ff)`}
-          />
-
-          {lineYArray.map((val, idx) => (
-            <Line
-              sy={horizon + at50 - val}
-              ey={horizon + at50 - val}
-              sx={0}
-              ex={screenWidth}
-              strokeWidth={idx / 10}
-              filter={`drop-shadow(${(horizTotal - idx) / 5}px ${
-                (horizTotal - idx) / 5
-              }px ${(horizTotal - idx) * 4}px #ff00ff) drop-shadow(-${
-                (horizTotal - idx) / 5
-              }px -${(horizTotal - idx) / 5}px ${horizTotal - idx}px #ff00ff)`}
-            />
-          ))}
-        </Svg>
+        <VertLines
+          width={screenWidth}
+          height={screenHeight}
+          horizon={horizon}
+        />
+        <HorizLines
+          width={screenWidth}
+          height={screenHeight}
+          horizon={horizon}
+        />
       </header>
     </div>
   );
