2a79f507d62f4cc55a2df89135cd1fc32694a809
Maximus Groves <max.groves@ncr.com>
2022-04-25 22:54:20 -0400

    adding file

---
+++ src/Horizon.js
@@ -0,0 +1,46 @@
+import { Svg, Line } from "react-svg-path";
+import "./App.css";
+import React from "react";
+import { useSpring, animated } from "react-spring";
+
+const Horizon = ({ width, height, horizon, mobile = false }) => {
+  const glowColor = "#ff00ff";
+
+  const [{ stroke }] = useSpring(() => ({
+    loop: true,
+    to: [
+      // { stroke: "#ffffff" },
+      { stroke: "#00ffff" },
+      { stroke: "#ff00ff" },
+      { stroke: "#ffff00" },
+    ],
+    from: { stroke: "#ffff00" },
+    reset: true,
+    config: { duration: 3000 },
+  }));
+
+  return (
+    <div style={{ position: "absolute", top: horizon, left: 0 }}>
+      <animated.div
+        style={{
+          //more transforms on desktop
+
+          strokeWidth: "2px",
+          filter: `
+                  drop-shadow(${8}px ${8}px ${16}px ${glowColor})
+                  drop-shadow(-${8}px ${8}px ${40}px ${glowColor})
+                  drop-shadow(4px -5px 5px ${glowColor})
+                  `,
+          stroke: stroke,
+          // stroke: "rgba(255,0,255,0.8)",
+        }}
+      >
+        <Svg width={width} height={height}>
+          <Line sy={0} ey={0} sx={0} ex={width || 0} />
+        </Svg>
+      </animated.div>
+    </div>
+  );
+};
+
+export default Horizon;
